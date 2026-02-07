eb591a38ccffe37d55d5dc12519b8c5a9e3a6d00
Riddhi Pachehara <Riddhipachehara@gmail.com>
2026-02-07 21:03:35 +0530

    Add insertion sort implementation in insertion.cpp
    Implement insertion sort algorithm and test it in main function.

--- src /insertion.cpp
+++ src /insertion.cpp
@@ -1,1 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void insertionSort(vector<int>& arr) {
+    int n = arr.size();
+    for(int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+
+        while(j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+int main() {
+    vector<int> arr = {9, 5, 1, 4, 3};
+    insertionSort(arr);
+
+    for(int x : arr) cout << x << " ";
+    return 0;
+}
 
