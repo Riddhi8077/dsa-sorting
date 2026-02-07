ee913122b32b4e684b9a75ebf05752ca557fbf61
Riddhi Pachehara <Riddhipachehara@gmail.com>
2026-02-07 21:01:26 +0530

    Implement bubble sort algorithm in bubble.cpp

--- src /bubble.cpp
+++ src /bubble.cpp
@@ -1,1 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void bubbleSort(vector<int>& arr) {
+    int n = arr.size();
+    for(int i = 0; i < n - 1; i++) {
+        for(int j = 0; j < n - i - 1; j++) {
+            if(arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+int main() {
+    vector<int> arr = {5, 3, 8, 4, 2};
+    bubbleSort(arr);
+
+    for(int x : arr) cout << x << " ";
+    return 0;
+}
 
