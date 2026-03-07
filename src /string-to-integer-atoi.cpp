2b923f83ab6de72fdc178c049c10c48cce9ae8d7
Riddhi Pachehara <Riddhipachehara@gmail.com>
2026-03-07 18:27:02 +0530

    Added solution for String to Integer (atoi) with overflow handling

---
+++ src /string-to-integer-atoi.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        int i = 0, n = s.size();
+        long result = 0;
+        int sign = 1;
+
+        // Skip leading spaces
+        while (i < n && s[i] == ' ') i++;
+
+        // Check sign
+        if (i < n && (s[i] == '+' || s[i] == '-')) {
+            if (s[i] == '-') sign = -1;
+            i++;
+        }
+
+        // Convert digits
+        while (i < n && isdigit(s[i])) {
+            result = result * 10 + (s[i] - '0');
+
+            if (sign * result > INT_MAX) return INT_MAX;
+            if (sign * result < INT_MIN) return INT_MIN;
+
+            i++;
+        }
+
+        return sign * result;
+    }
+};
