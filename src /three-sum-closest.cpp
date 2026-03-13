85fa1eece8e0db5cc7854634531ce97487a84e58
Riddhi Pachehara <Riddhipachehara@gmail.com>
2026-03-13 21:48:16 +0530

    Correct threeSumClosest logic for target comparison
    Fix logic in threeSumClosest function to handle cases where currentSum is greater than target.

--- src /three-sum-closest.cpp
+++ src /three-sum-closest.cpp
@@ -8,8 +8,9 @@ public:
     int threeSumClosest(vector<int>& nums, int target) {
         sort(nums.begin(), nums.end());
         int n = nums.size();
-
+        
         int closestSum = nums[0] + nums[1] + nums[2];
+
         for(int i = 0; i < n - 2; i++) {
             int left = i + 1;
             int right = n - 1;
@@ -24,18 +25,15 @@ public:
                 if(currentSum < target) {
                     left++;
                 }
-                else if(currentSum < target) {
+                else if(currentSum > target) {
                     right--;
                 }
                 else {
                     return currentSum;
                 }
-                
             }
-            
         }
 
         return closestSum;
-        
     }
 };
