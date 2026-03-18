adb23e22224d9090107264f38ab61e43e9573fb6
Riddhi Pachehara <Riddhipachehara@gmail.com>
2026-03-18 23:32:48 +0530

    Added O(n) hash set solution for Longest Consecutive Sequence

---
+++ src /longest-consecutive-sequence.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int> s(nums.begin(), nums.end());
+        int longest = 0;
+
+        for (int num : s) {
+            // start only if it's the beginning of a sequence
+            if (!s.count(num - 1)) {
+                int current = num;
+                int length = 1;
+
+                while (s.count(current + 1)) {
+                    current++;
+                    length++;
+                }
+
+                longest = max(longest, length);
+            }
+        }
+
+        return longest;
+    }
+};
